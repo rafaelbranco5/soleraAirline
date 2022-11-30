e3847374fee7b9f9ae00d2187f853258116a11f7
Rafael Branco <Rafael.Branco@solera.com>
2022-11-30 10:11:18 +0000

    Created test for get scales API call, apllyed data providers to scales and flight by airline

--- src/test/java/com/solera/airline/controller/FlightControllerTest.java
+++ src/test/java/com/solera/airline/controller/FlightControllerTest.java
@@ -10,6 +10,7 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Optional;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -32,20 +33,47 @@ public class FlightControllerTest {
 		assertThat(string).startsWith("application/json");
 	}
 
-	@Test // RE-DO
-	public void ensureThatApiCallFlightByAirlineReturnsCode200() throws Exception {
-		String airline = "tap";
+	@DataProvider(name = "flightByAirline")
+	public String[] dataForFlightByAirlineTest() {
+		return new String[] {"ryanair", "tap", "easyjet"};
+	}
+	@Test(dataProvider = "flightByAirline")
+	public void ensureThatApiCallFlightByAirlineReturnsCode200(String airline) throws Exception {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/airline/" + airline)).build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		assertThat(response.statusCode()).isEqualTo(200);
   }
-	@Test
-	public void ensureThatUserApiCallFlightsByAirlineReturnsJsonContent() throws Exception {
-		String airline = "tap";
+	@Test(dataProvider = "flightByAirline")
+	public void ensureThatUserApiCallFlightsByAirlineReturnsJsonContent(String airline) throws Exception {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/airline/" + airline)).build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		Optional<String> firstValue = response.headers().firstValue("Content-Type");
 		String string = firstValue.get();
 		assertThat(string).startsWith("application/json");
+	}
+
+	// Was having problems trying ot pass an array of int, but since URI.create takes in a String decided to pass the values as a String array
+	@DataProvider(name = "flightsByNumberOfScales")
+	public String[] dataForFlightsByScalesTest() {
+		return new String[] {"0", "1", "2"};
+	}
+	@Test(dataProvider = "flightsByNumberOfScales")
+	public void ensureThatUserApiCallScalesReturnsCode200(String numOfScales) throws Exception {
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/scales/" + numOfScales)).build();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		assertThat(response.statusCode()).isEqualTo(200);
+	}
+	@Test(dataProvider = "flightsByNumberOfScales")
+	public void ensureThatUserApiCallScalesReturnsJsonContent(String numOfScales) throws Exception {
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/scales/" + numOfScales)).build();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		Optional<String> firstValue = response.headers().firstValue("Content-Type");
+		String string = firstValue.get();
+		assertThat(string).startsWith("application/json");
+	}
+	
+	@DataProvider(name = "flightByLuggageCheck")
+	public String[] dataForFlightByLuggageTest() {
+		return new String[] {"true", "false"};
 	}
 }
